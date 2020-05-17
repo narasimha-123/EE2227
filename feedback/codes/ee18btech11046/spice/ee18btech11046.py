dc1b8dfb3c1ff05f0ed5ffee38214651c5d5c1a2
narasimha-123 <47531798+narasimha-123@users.noreply.github.com>
2020-05-17 17:07:44 +0530

    Update ee18btech11046.py

--- feedback/codes/ee18btech11046/spice/ee18btech11046.py
+++ feedback/codes/ee18btech11046/spice/ee18btech11046.py
@@ -13,10 +13,10 @@ plt.grid()
 plt.xlabel("time in sec")
 plt.ylabel("Output Voltage")
 plt.title('Closed loop system Output Voltage')
-plt.show()
+#plt.show()
 
 
 #if using termux
-#plt.savefig('./figs/ee18btech11046/ee18btech11046_spice_result.pdf')
-#plt.savefig('./figs/ee18btech11034/ee18btech11046_spice_result.eps')
-#subprocess.run(shlex.split("termux-open ./figs/ee18btech11046/ee18btech11046_spice_result.pdf"))
+plt.savefig('./figs/ee18btech11046/ee18btech11046_spice_result.pdf')
+plt.savefig('./figs/ee18btech11046/ee18btech11046_spice_result.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11046/ee18btech11046_spice_result.pdf"))
